d1ea9a6c37c77977a9688b7fbf4450309a28c9d6
Arseniy <Arseniy@DESKTOP-ICDC5TR>
2021-03-05 15:17:14 +0300

    Calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,73 +1,103 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Calculator
 {
-
     class Program
     {
-        static int GetNextOperant(string vvod)
+        static string Check()
         {
-            int result=0;
-            bool FirstCheck = int.TryParse(vvod, out result);
-            if (!FirstCheck)
+            exite1:
+            Console.WriteLine("Введите выражение");
+            string vvod = Console.ReadLine();
+            char operant = '0';
+            int stringLength = vvod.Length;
+            for (int counter = 0; counter < stringLength; counter++)
             {
-                char operant='0';
-                int counter = 0;
-                int StringLength = vvod.Length;
-                for (; counter < StringLength; counter++)
+                if (vvod[counter] == '+' || vvod[counter] == '-' || vvod[counter] == '*' || vvod[counter] == '/')
                 {
-                    if (vvod[counter] == '+' || vvod[counter] == '-')
+                    if (operant == '1')
                     {
-                        char[] left = new char[counter];
-                        char[] right = new char[StringLength - counter];
-                        for (int i = 0; i < counter; i++)
-                        {
-                            left[i] = vvod[i];
-                        }
-                        int BufCounter = counter + 1;
-                        for (int i = 0; BufCounter < StringLength; BufCounter++)
-                        {
-                            right[i] = vvod[BufCounter];
-                            i++;
-                        }
-                        int resultLeft;
-                        int resultRight;
-                        bool check1 = int.TryParse(left, out resultLeft);
-                        bool check2 = int.TryParse(right, out resultRight);
-                        if (check1 == true && check2 == true)
-                        {
-                            if (vvod[counter] == '+')
-                            {
-                                result = resultLeft + resultRight;
-                                operant = '+';
-                            }
-                            else
-                            {
-                                result = resultLeft - resultRight;
-                                operant = '-';
-                            }
-                        }
-                        else
-                        {
-                            string left1 = new string(left);
-                            string right1 = new string(right);
-                            if (vvod[counter] == '+')
-                            {
-                                result = GetNextOperant(left1) + GetNextOperant(right1);
-                                operant = '+';
-                            }
-                            else
-                            {
-                                result = GetNextOperant(left1) - GetNextOperant(right1);
-                                operant = '-';
-                            }
-
-                        }
+                        Console.WriteLine("Введено 2 знака операции подряд,повторите ввод");
+                        goto exite1;
                     }
                     else
+                        operant = '1';
+                }
+                else
+                {
+                    operant = '0';
+                        switch(vvod[counter])
+                        {
+                            case '0':
+                            case '1':
+                            case '2':
+                            case '3':
+                            case '4':
+                            case '5':
+                            case '6':
+                            case '7':
+                            case '8':
+                            case '9':
+                                break;
+                            default:
+                            Console.WriteLine("Был введен неизвестный знак, повторите ввод");
+                            goto exite1;
+                        }
+                }
+
+            }
+            return vvod;
+        }
+            
+            static double GetNextOperant(string vvod)
+            {
+                double result = 0;
+               bool FirstCheck = double.TryParse(vvod, out result);
+                if (!FirstCheck)
+                {
+                    int StringLength = vvod.Length;
+                    for (int counter = 0; counter < StringLength; counter++)
                     {
-  
-                        if (vvod[counter] == '*' || vvod[counter] == '/')
+                        if (vvod[counter] == '+' || vvod[counter] == '-')
+                        {
+
+                            char[] left = new char[counter];
+                            char[] right = new char[StringLength - counter];
+                            for (int i = 0; i < counter; i++)
+                            {
+                                left[i] = vvod[i];
+                            }
+                            for (int i = counter + 1, j = 0; i < StringLength; i++, j++)
+                            {
+                                right[j] = vvod[i];
+                            }
+                            double resultLeft;
+                            double resultRight;
+                            bool check1 = double.TryParse(left, out resultLeft);
+                            bool check2 = double.TryParse(right, out resultRight);
+                            string Left = new string(left);
+                            string Right = new string(right);
+                            if (check1 && check2)
+                            {
+                                if (vvod[counter] == '+')
+                                    return result = resultLeft + resultRight;
+                                else
+                                    return result = resultLeft - resultRight;
+                            }
+                            else
+                            {
+                                if (vvod[counter] == '+')
+                                    return result = GetNextOperant(Left) + GetNextOperant(Right);
+                                else
+                                    return result = GetNextOperant(Left) - GetNextOperant(Right);
+                            }
+                        }
+                    }
+
+                    for(int counter=0;counter<StringLength;counter++)
+                    {
+                        if(vvod[counter]=='*' || vvod[counter]=='/')
                         {
                             char[] left = new char[counter];
                             char[] right = new char[StringLength - counter];
@@ -75,55 +105,43 @@ namespace Calculator
                             {
                                 left[i] = vvod[i];
                             }
-                            int BufCounter = counter + 1;
-                            for (int i = 0; BufCounter < StringLength; BufCounter++)
+                            for (int i = counter + 1, j = 0; i < StringLength; i++, j++)
                             {
-                                right[i] = vvod[BufCounter];
-                                i++;
+                                right[j] = vvod[i];
                             }
-                            int resultLeft;
-                            int resultRight;
-                            bool check1 = int.TryParse(left, out resultLeft);
-                            bool check2 = int.TryParse(right, out resultRight);
-                            if (check1 == true && check2 == true)
+                            double resultLeft;
+                            double resultRight;
+                            bool check1 = double.TryParse(left, out resultLeft);
+                            bool check2 = double.TryParse(right, out resultRight);
+                            string Left = new string(left);
+                            string Right = new string(right);
+                            if (check1 && check2)
                             {
                                 if (vvod[counter] == '*')
-                                {
-                                    result = resultLeft * resultRight;
-                                }
+                                    return result = resultLeft * resultRight;
                                 else
-                                {
-                                    result = resultLeft / resultRight;
-                                }
+                                    return result = resultLeft / resultRight;
                             }
                             else
                             {
-                                string left1 = new string(left);
-                                string right1 = new string(right);
                                 if (vvod[counter] == '*')
-                                {
-                                    result = GetNextOperant(left1) * GetNextOperant(right1);
-                                }
+                                    return result = GetNextOperant(Left) * GetNextOperant(Right);
                                 else
-                                {
-                                    result = GetNextOperant(left1) / GetNextOperant(right1);
-                                }
-
+                                    return result = GetNextOperant(Left) / GetNextOperant(Right);
                             }
                         }
                     }
+
                 }
+                return result;
             }
-            else
-                result = int.Parse(vvod);
-            return result;
-        }
-        public static void Main(string[] args)
-          {
-            Console.WriteLine("Введите выражение");
-            string vvod = Console.ReadLine();
-           int result = GetNextOperant(vvod);
-            Console.WriteLine(result);
-          }
+            public static void Main(string[] args)
+            {
+                string vvod = Check();
+                double result = GetNextOperant(vvod);
+                Console.WriteLine(result);
+            }
     }
 }
+
+
